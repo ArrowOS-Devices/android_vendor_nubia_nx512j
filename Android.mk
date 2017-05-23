60c2884ed09dcfd916231b9ea64ab105d4e43225
Bauuuuu <abansode@outlook.com>
2017-05-23 16:11:47 +0200

    Move filesmap to device

--- Android.mk
+++ Android.mk
@@ -122,6 +122,6 @@ ifeq ($(LOCAL_PATH)/radio, $(wildcard $(LOCAL_PATH)/radio))
 RADIO_FILES := $(wildcard $(LOCAL_PATH)/radio/*)
 $(foreach f, $(notdir $(RADIO_FILES)), \
     $(call add-radio-file,radio/$(f)))
-$(call add-radio-file,radio/filesmap)
+$(call add-radio-file,../../../device/nubia/nx512j/radio/filesmap)
 endif
 endif
